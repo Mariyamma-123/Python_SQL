0470f4a9aee6d6dbe4616e2de326e2ade43b34d2
Mariyamma <karanimariyamma@gmail.com>
2025-08-25 19:57:35 +0530

    18-8-25(1).py

--- 18-8-25(1).py
+++ 18-8-25(1).py
@@ -1,11 +1,11 @@
-Python 3.12.3 | packaged by Anaconda, Inc. | (main, May  6 2024, 19:42:21) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #write a program to check it is a positive or negative number
-... n=int(input("enter a number : "))
-... if n > 0:
-...     print("positive number")
-... elif n < 0:
-...     print("Negative number")
-... else:
-...     print("zero")
-... #input : enter a number : 8
+#write a program to check it is a positive or negative number
+n=int(input("enter a number : "))
+if n > 0:
+    print("positive number")
+elif n < 0:
+    print("Negative number")
+else:
+    print("zero")
+#input : enter a number : 3
+#output : positive number
+
