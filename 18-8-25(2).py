1015930349ebbd0b2c3c365abfa683521ec0e361
Mariyamma <karanimariyamma@gmail.com>
2025-08-25 20:02:29 +0530

    Add files via upload

--- 18-8-25(2).py
+++ 18-8-25(2).py
@@ -1,11 +1,11 @@
-#program to find sum of n natural numbers.
-n=int(input("enter a number : "))
-i=1
-s=0
-while(i<=n):
-    s+=i
-    i+=1
-print(s)
-'''input : enter a number : 6
-output : 21'''
-
+Python 3.12.3 | packaged by Anaconda, Inc. | (main, May  6 2024, 19:42:21) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #program to find sum of n natural numbers.
+... n=int(input("enter a number : "))
+... i=1
+... s=0
+... while(i<=n):
+...     s+=i
+...     i+=1
+... print(s)
+... '''input : enter a number : 6
