891dbed1803e7ce55d0e4dada28615bbab1696a5
Mariyamma <karanimariyamma@gmail.com>
2025-08-25 20:05:33 +0530

    Add files via upload

---
+++ 18-8-25(3).py
@@ -0,0 +1,9 @@
+Python 3.12.3 | packaged by Anaconda, Inc. | (main, May  6 2024, 19:42:21) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #write a program to check if it is a even or odd number.
+... n=int(input("enter a number : "))
+... if n % 2 == 0:
+...     print("Even")
+... else:
+...     print("Odd")
+... #input : enter a number : 7
